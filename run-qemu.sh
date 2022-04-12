2f05de67977fc94fd10765ea62f6eeeacd4d1106
Norio Goda <noriov@tutanota.com>
2022-04-13 05:16:40 +0900

    Written to use "cargo objcopy".

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,22 +1,10 @@
 #! /bin/sh
 
 TARGET="x86_64-unknown-none"
-
-DEVSYS=`rustup toolchain list | grep nightly-x86_64 | sed -e 's/^nightly-//' | awk ' { print $1 } '`
 NAME=`grep name Cargo.toml | cut -d= -f2 | sed -e 's/[ "]*//g'`
-
-TOOLCHAIN_DIR="$HOME/.rustup/toolchains/nightly-$DEVSYS"
-OBJCOPY="$TOOLCHAIN_DIR/lib/rustlib/$DEVSYS/bin/llvm-objcopy"
-
-INPUT="target/$TARGET/debug/$NAME"
 OUTPUT="target/$TARGET/debug/$NAME.bin"
 
-$OBJCOPY \
-	-I elf64-x86-64 \
-	-O binary \
-	--binary-architecture=i386:x86-64 \
-	$INPUT \
-	$OUTPUT
+cargo objcopy --bin nostd_env -- -O binary $OUTPUT
 
 qemu-system-x86_64 \
 	-drive format=raw,file=$OUTPUT \

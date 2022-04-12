1242b4b8a0906fb9dfd76e8390ddaac0d054c119
Norio Goda <noriov@tutanota.com>
2022-04-12 19:09:39 +0900

    src/bios/sysv_abi.s is renamed to src/bios/wrapper_sysv.s

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/bios/lmboot0.s");
     println!("cargo:rerun-if-changed=src/bios/lmbios1.s");
-    println!("cargo:rerun-if-changed=src/bios/sysv_abi.s");
+    println!("cargo:rerun-if-changed=src/bios/wrapper_sysv.s");
     println!("cargo:rerun-if-changed=config/x86_64-unknown-none.json");
     println!("cargo:rerun-if-changed=config/x86_64-unknown-none.ld");
     println!("cargo:rerun-if-changed=build.rs");

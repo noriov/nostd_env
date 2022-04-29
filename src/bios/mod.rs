fc6020e3780614d7a7e4d14ee789f81211d8c38f
Norio Goda <noriov@tutanota.com>
2022-04-30 05:02:28 +0900

    Struct bios::Int10h0eh is added.

--- src/bios/mod.rs
+++ src/bios/mod.rs
@@ -1,5 +1,8 @@
 pub mod asm;
 pub mod api;
 pub mod ffi;
+pub mod int10h0eh;
 
 pub use api::{LmbiosRegs, check_stack_usage, get_boot_drive_id};
+
+pub use int10h0eh::Int10h0eh;
--- src/text_writer.rs
+++ src/text_writer.rs
@@ -10,27 +10,16 @@ use crate::bios;
 pub struct TextWriter;
 
 impl TextWriter {
-    pub fn write_byte(&mut self, byte: u8) {
-	unsafe {
-	    // INT 10h AH=0Eh (Teletype Output)
-	    // AL: Character, BH: Page Number, BL: Color
-	    // cf. https://en.wikipedia.org/wiki/INT_10H
-	    bios::LmbiosRegs {
-		fun: 0x10,			// INT 10h AH=0Eh
-		eax: 0x0E00 | byte as u32,	// AL: Character = `byte`
-		ebx: 0x000F,			// BL: Color = 15 (white)
-		..Default::default()
-	    }.call();
-	}
-    }
-
     pub fn write_ascii_printables(&mut self, utf8_str: &str) {
 	for byte in utf8_str.bytes() {
-	    self.write_byte(
+	    let ch =
 		match byte {
 		    0x20 ..= 0x7E | b'\n' | b'\r' => byte,
 		    _ => b'.'
-		});
+		};
+	    let page_number = 0;
+	    let color = 15; // White
+	    bios::Int10h0eh::call(ch, page_number, color);
 	}
     }
 }
@@ -61,7 +50,7 @@ macro_rules! print {
 }
 
 pub fn _text_print(args: fmt::Arguments) {
-    use core::fmt::Write;
+    use fmt::Write;
     let mut text_writer = TextWriter;
     text_writer.write_fmt(args).unwrap();
 }

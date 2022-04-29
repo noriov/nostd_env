f79454301c33e28e1769c7879713730c9e789597
Norio Goda <noriov@tutanota.com>
2022-04-30 04:45:36 +0900

    Assembly language files are moved from src/bios to src/bios/asm.

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/bios/debug_helper.s");
-    println!("cargo:rerun-if-changed=src/bios/lmboot0.s");
-    println!("cargo:rerun-if-changed=src/bios/lmbios1.s");
-    println!("cargo:rerun-if-changed=src/bios/wrapper_sysv.s");
+    println!("cargo:rerun-if-changed=src/bios/asm/debug_helper.s");
+    println!("cargo:rerun-if-changed=src/bios/asm/lmboot0.s");
+    println!("cargo:rerun-if-changed=src/bios/asm/lmbios1.s");
+    println!("cargo:rerun-if-changed=src/bios/asm/wrapper_sysv.s");
     println!("cargo:rerun-if-changed=config/x86_64-unknown-none.json");
     println!("cargo:rerun-if-changed=config/x86_64-unknown-none.ld");
 }
--- src/bios/mod.rs
+++ src/bios/mod.rs
@@ -1,13 +1,5 @@
+pub mod asm;
 pub mod api;
 pub mod ffi;
 
 pub use api::{LmbiosRegs, get_boot_drive_id};
-
-
-use core::arch::global_asm;
-
-global_asm!(include_str!("lmboot0.s"), options(att_syntax));
-global_asm!(include_str!("lmbios1.s"), options(att_syntax));
-global_asm!(include_str!("wrapper_sysv.s"), options(att_syntax));
-
-global_asm!(include_str!("debug_helper.s"), options(att_syntax));

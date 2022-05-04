0353f2487f0c517581a98e346f84f26e7524cfcf
Norio Goda <noriov@tutanota.com>
2022-05-05 05:42:28 +0900

    Comments are updated.

--- src/x86/mod.rs
+++ src/x86/mod.rs
@@ -1,9 +1,17 @@
-pub mod halt_forever;
-pub mod x86_far_ptr;
-pub mod x86_get_addr;
+/*!
 
-pub use self::halt_forever::halt_forever;
-pub use self::x86_far_ptr::X86FarPtr;
-pub use self::x86_get_addr::X86GetAddr;
+Provides X86-related utilities.
 
+ */
+
+
+#[doc(hidden)] pub mod halt_forever;
+#[doc(hidden)] pub mod x86_far_ptr;
+#[doc(hidden)] pub mod x86_get_addr;
+
+#[doc(inline)] pub use self::halt_forever::halt_forever;
+#[doc(inline)] pub use self::x86_far_ptr::X86FarPtr;
+#[doc(inline)] pub use self::x86_get_addr::X86GetAddr;
+
+/// The Carry Flag (CF) in the FLAGS register.
 pub const FLAGS_CF: u16 = 0x0001;
